a5f0ae0c7539427b9c66bd271f42e6817494f4e2
Thais Freire <thaism.freire@outlook.com>
2021-05-28 09:02:43 -0300

    list ok

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,25 @@
+class ListsController < ApplicationController
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+    # @bookmark = list.bookmarker
+    # bookmark = Bookmark.new
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to @list, notice: "List was successfully created!"
+    else
+      render :new
+    end
+  end
+end
